bed4cb29664ac91a652fa1567248aa1ba2b666ac
Alexandra Bozzini <alexandrabozzini@zipcodersmbp74.lan>
2019-05-10 12:04:48 -0400

    Imported bootswatch theme and added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderbarComponent } from './headerbar/headerbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderbarComponent
   ],
   imports: [
     BrowserModule
